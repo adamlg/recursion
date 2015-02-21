1b4ae38e375e76ed9b47dd320912439236525471
Adam <adam.l.guerra@gmail.com>
2015-02-21 01:58:37 -0600

    Completed Assignment 3.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  function stringify(input) {
+    var type = typeof input;
+    if (type == 'number' || type == 'boolean' || input === null || input === undefined) {
+      return String(input);
+    };
+
+    if (type == 'string') {
+      return '"'+input+'"';
+    };
+
+    if (type == 'function') {
+      return undefined;
+    };
+
+    if (Array.isArray(input)) {
+      var results = '[';
+      for (var i=0; i<input.length; i++) {
+        results += stringify(input[i]);
+        if (i<input.length-1) {
+          results +=',';
+        };
+      };
+      results += ']';
+      return results;
+    };
+
+    if (type == 'object' && !Array.isArray(input)) {
+      var results = '{';
+      var keys = Object.keys(input);
+      for (var i=0; i<keys.length; i++) {
+        if (input[keys[i]]===undefined || typeof input[keys[i]]=='function') {
+          continue;
+        };
+        results += stringify(keys[i])+':'+stringify(input[keys[i]]);
+        if (i<keys.length-1) {
+          results +=',';
+        };
+      };
+      results += '}';
+      return results;
+    };
+
+  };
+
+  return stringify(obj);
 };
+
+console.log(stringifyJSON({}));
